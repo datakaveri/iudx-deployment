4cdaa77efe3af1a31d5feddfc3f7eefc9731d16d
Abhilash Venkatesh <abhilashnitk3@gmail.com>
2020-08-27 18:29:28 +0530

    modified promtail-config, manager-infra.yml
    - removed label "level" from promtail, will create more streams
    - corrected prometheus service in manager-infra.yml and install.sh

--- single-node/monitoring-stack/install.sh
+++ single-node/monitoring-stack/install.sh
@@ -3,6 +3,7 @@ export admin_passwd=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9-_!@#$%^&*()_+{}|:<>?=
 echo -n "$admin_passwd" | docker secret  create grafana_admin_passwd -
 echo "------------Grafana_details------------" >secrets.txt
 echo "admin details : username is admin and password is $admin_passwd" >>secrets.txt
+mkdir  /tmp/metrics-targets
 docker stack deploy -c manager-infra.yml mon_stack
 sleep 5 && echo "awake"
 ./grafana_users_install.sh 2
