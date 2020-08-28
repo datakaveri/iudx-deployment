88eb7e222c8b6979b2ece4136d35c64f7e92f342
Abhilash V <abhilashnitk3@gmail.com>
2020-08-28 12:39:38 +0530

    Update grafana_users_install.sh

--- single-node/monitoring-stack/grafana_users_install.sh
+++ single-node/monitoring-stack/grafana_users_install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #Requirements : jq, curl tools
 
-# export metrics_passwd=`cat /dev/urandom | tr -dc 'a-zA-Z0-9-_!@#$%^&*()_+{}|:<>?=' | head -c 12`
-# echo "mertics auth details: username is user_met and password is $metrics_passwd" >> secrets.txt
+apt update &&  apt install jq curl 
 
 create_users() {
-	url="http://localhost:3000/api/admin/users"
+	url="http://grafana:3000/api/admin/users"
 	for ((i = 1; i <= $1; i++)); do
 		passwd=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9-_!@#$%^&*()_+{}|:<>?=' | head -c 12)
 		json_body=$(echo "{\"name\":\"User$i\",\"email\":\"user$i@datakaveri.org \", \"OrgId\": $2, \"login\":\"user$i\", \"password\":\"$passwd\"}" | jq .)
@@ -22,7 +21,7 @@ create_users() {
 
 # update name of  organisation
 ((org_id = 1))
-org_url="http://localhost:3000/api/orgs/$org_id"
+org_url="http://grafana:3000/api/orgs/$org_id"
 org_res=$(curl -s -XPUT -H 'Content-Type: application/json' --user admin:"$admin_passwd" -d "$(printf '{"name":"IUDX" }' | jq .)" "$org_url")
 
 # create users
@@ -30,7 +29,7 @@ org_res=$(curl -s -XPUT -H 'Content-Type: application/json' --user admin:"$admin
 create_users "$no_of_users" "$org_id"
 
 # update access roles (by default the added users have viewer roles)
-access_url="http://localhost:3000/api/orgs/$org_id/users"
+access_url="http://grafana:3000/api/orgs/$org_id/users"
 access_res=$(curl -s -XPATCH -H 'Content-Type: application/json' --user admin:"$admin_passwd" -d "$(printf '{"role":"Editor" }' | jq .)" "$access_url/2")
 if [[ $(echo "$access_res" | jq .message) == '"Organization user updated"' ]]; then
 
@@ -39,5 +38,5 @@ else
 	echo "user 1 could not be made editor and so  has Viewer access" >>secrets.txt
 fi
 
-#unset metrics_passwd
+
 echo "please find details of users, admin in secrets.txt file"
