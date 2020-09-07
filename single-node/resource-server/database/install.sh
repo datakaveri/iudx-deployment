7f26d280e0204b83361dec4584650a98f401443a
Abhilash Venkatesh <abhilashnitk3@gmail.com>
2020-09-07 20:26:45 +0530

    Logstash keystore
    - logstash keystore file generation using logstash_setup.sh
    - logstash keystore contains credentials: rabbitmq username, passwd

--- single-node/resource-server/database/install.sh
+++ single-node/resource-server/database/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 docker secret create  kibana_tls_cert.pem secrets/kibana_tls_cert.pem
 docker secret create kibana_tls_key.pem secrets/kibana_tls_key.pem
+docker run -it --rm -v $PWD/:/root  docker.elastic.co/logstash/logstash:7.8.1 /bin/bash /root/logstash_setup.sh
+docker secret create logstash_keystore secrets/logstash.keystore
 docker stack deploy -c database_stack.yml database
