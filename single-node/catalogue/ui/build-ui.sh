8a9c8dde0a76427cfe56c95e84be435570d5d2f1
Abhilash Venkatesh <abhilashnitk3@gmail.com>
2020-10-08 13:04:17 +0530

    update cat.conf and build-ui
    - fixed issue of limit_req while loading a page with inclusion of burst
    - optimised ui build using multi-stage image build

--- single-node/catalogue/ui/build-ui.sh
+++ single-node/catalogue/ui/build-ui.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t iudx/cat-ui:latest -f ui.dockerfile .
-docker run -v /usr/share/app/dk-customer-ui:/dist -i -t iudx/cat-ui:latest /bin/sh -c "cp -r dist ../"
+docker run -v /usr/share/app/:/dist -i -t iudx/cat-ui:latest /bin/sh -c "cp -r dist/ ../"
--- single-node/catalogue/ui/docker/build-ui.sh
+++ single-node/catalogue/ui/docker/build-ui.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e 
 #mountpoint of UI files is /usr/share:/mnt
-git clone https://github.com/datakaveri/dk-customer-ui.git /dk-customer-ui
-cd /dk-customer-ui
-echo n | npm install
-ng build --configuration=production
-mkdir -p /mnt/app/dk-customer-ui
-cp -r dist/* /mnt/app/dk-customer-ui/
+mkdir -p /mnt/app/
+cp -r dk-customer-ui/ /mnt/app/
+echo "successfully copied the files"
