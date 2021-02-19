131d734f8e8bd186e0446618272aa26913e0f24e
Abhilash V <abhilashnitk3@gmail.com>
2021-02-19 12:36:18 +0530

    K8s-deployment first commit
    - adding K8s-cluster specific things - minikube, Rancher
    - adding databroker (RMQ) cluster deplyment in K8s

---
+++ K8s-deployment/resource-server/databroker/deploy-dependencies.sh
@@ -0,0 +1,10 @@
+kubectl apply -f namespace.yaml
+kubectl create secret generic certs --from-file=../../secrets/pki/ca-cert.pem --from-file=../../secrets/pki/server-cert.pem --from-file=../../secrets/pki/server-key.pem -n rabbitmq-test
+kubectl create secret generic rabbitmq-erlang-cookie --from-file=../../secrets/passwords/rabbitmq-erlang-cookie -n rabbitmq-test
+kubectl create secret generic rabbitmq-definition-file --from-file=../../secrets/passwords/rabbitmq-definitions.json -n rabbitmq-test
+kubectl apply -f rbac.yaml
+kubectl apply -f configmap.yaml
+kubectl apply -f external-client-service.yaml
+kubectl apply -f internal-client-service.yaml
+kubectl apply -f headless-service.yaml
+
