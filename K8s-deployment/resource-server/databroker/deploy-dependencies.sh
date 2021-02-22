9420e7a324ee4b883cacaee21932fb55c41a8b14
Abhilash V <abhilashnitk3@gmail.com>
2021-02-22 22:24:18 +0530

    Adding Cluster autoscaler documentation

--- K8s-deployment/resource-server/databroker/deploy-dependencies.sh
+++ K8s-deployment/resource-server/databroker/deploy-dependencies.sh
@@ -1,7 +1,7 @@
 kubectl apply -f namespace.yaml
 kubectl create secret generic certs --from-file=../../secrets/pki/ca-cert.pem --from-file=../../secrets/pki/server-cert.pem --from-file=../../secrets/pki/server-key.pem -n rabbitmq-test
-kubectl create secret generic rabbitmq-erlang-cookie --from-file=../../secrets/passwords/rabbitmq-erlang-cookie -n rabbitmq-test
-kubectl create secret generic rabbitmq-definition-file --from-file=../../secrets/passwords/rabbitmq-definitions.json -n rabbitmq-test
+kubectl create secret generic rabbitmq-erlang-cookie --from-file=../../secrets/credentials/rabbitmq-erlang-cookie -n rabbitmq-test
+kubectl create secret generic rabbitmq-definition-file --from-file=../../secrets/credentials/rabbitmq-definitions.json -n rabbitmq-test
 kubectl apply -f rbac.yaml
 kubectl apply -f configmap.yaml
 kubectl apply -f external-client-service.yaml
