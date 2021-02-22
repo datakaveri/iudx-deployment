f8f22c5145271adcfe651c36bfbfb29292b05bde
Abhilash V <abhilashnitk3@gmail.com>
2021-02-22 15:15:02 +0530

    Fixes in cat-ui build(#20)
    1) little optimization and fixes in dockerfile
    2) pushing the image to the private repository.
    3) update the cat-ui docker image

--- single-node/catalogue/ui/docker/docker-build.sh
+++ single-node/catalogue/ui/docker/docker-build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # --no-cache maybe removed once this is shifted to UI repo (i.e. not git clone  command)
-docker build --no-cache -t abhiurn/cat-ui:latest -f docker/Dockerfile  . 
-docker push abhiurn/cat-ui:latest 
-
+docker build --no-cache  -t dockerhub.iudx.io/iudx/cat-ui:latest -f docker/Dockerfile  .
+docker push dockerhub.iudx.io/iudx/cat-ui:latest
 
