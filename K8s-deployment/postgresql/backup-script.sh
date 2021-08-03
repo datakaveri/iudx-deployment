69811275168f5f13bd98459a269cffc14168a013
Abhilash <abhilashnitk3@gmail.com>
2021-08-03 04:27:24 +0000

    changing backup-s3  secret structure

--- K8s-deployment/postgresql/backup-script.sh
+++ K8s-deployment/postgresql/backup-script.sh
@@ -2,5 +2,5 @@
 
 timestamp=$(date -I'seconds')
 
-PGPASSWORD="$POSTGRES_PASSWORD" pg_dumpall -h psql-postgresql-ha-pgpool -p 5432 -U postgres | gzip | s3cmd -c /s3cmd/.s3cfg put - s3://$S3_BUCKET_NAME/postgres-backups/psql-backups-$timestamp.sql.gz
+PGPASSWORD="$POSTGRES_PASSWORD" pg_dumpall -h psql-postgresql-ha-pgpool -p 5432 -U postgres | gzip | s3cmd -c /s3cmd/.s3cfg put - s3://$S3_BUCKET_NAME/psql-backups-$timestamp.sql.gz
 echo "psql full backup taken successfully at $timestamp"
--- K8s-deployment/postgresql/create-secrets.sh
+++ K8s-deployment/postgresql/create-secrets.sh
@@ -13,11 +13,11 @@ merge_secret() {
 
 }
 
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*()+{}|:<>?=' | head -c 16) > secrets/postgresql-password
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*()+{}|:<>?=' | head -c 16) > secrets/repmgr-password
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*()+{}|:<>?=' | head -c 16) > secrets/postgres-auth-password
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*()+{}|:<>?=' | head -c 16) > secrets/postgres-rs-password
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*()+{}|:<>?=' | head -c 16) > secrets/postgres-keycloak-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/postgresql-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/repmgr-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/postgres-auth-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/postgres-rs-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/postgres-keycloak-password
 
 create_secret psql-passwords postgresql-password
 merge_secret psql-passwords repmgr-password
@@ -31,37 +31,8 @@ echo -n "$(cat secrets/postgresql-password);$(cat secrets/postgres-rs-password);
 create_secret pgpool-auth usernames
 merge_secret pgpool-auth passwords
 
-#create_secret backup-credentials postgresql-password
-#merge_secret backup-credentials aws-s3-access-id
-#merge_secret backup-credentials aws-s3-access-key
+kubectl create secret generic backup-s3-secret  --dry-run=client --from-file=S3_ACCESS_ID=./secrets/s3-access-id -n postgres  -o yaml | kubeseal --cert ../K8s-cluster/sealed-secrets/sealed-secrets-pub.pem  --format yaml > sealed-secrets/backup-s3-secret.yaml
 
-cat << EOF > sealed-secrets/backup-s3-cfg.yaml
-apiVersion: bitnami.com/v1alpha1
-kind: SealedSecret
-metadata:
-  creationTimestamp: null
-  name: backup-s3-cfg
-  namespace: postgres
-spec:
-  encryptedData:
-    aws-s3-access-id: "AgA+dujiBo..."
-    aws-s3-access-key: "AgCC4/FvhZ..."
-  template:
-    metadata:
-      creationTimestamp: null
-      name: backup-s3-cfg
-      namespace: postgres
-    data:
-     s3cfg : |
-       [default]
-       access_key = {{ index . "aws-s3-access-id" }}
-       secret_key = {{ index . "aws-s3-access-key" }}
-       bucket_location = ap-south-1
-       host_base = s3.ap-south-1.amazonaws.com
-       host_bucket = %(bucket)s.s3.ap-south-1.amazonaws.com
-       use_https = true
-EOF
+kubectl create secret generic backup-s3-secret  --dry-run=client --from-file=S3_ACCESS_KEY=./secrets/s3-access-key -n postgres  -o yaml | kubeseal --cert ../K8s-cluster/sealed-secrets/sealed-secrets-pub.pem  --format yaml --merge-into  sealed-secrets/backup-s3-secret.yaml
 
-merge_secret backup-s3-cfg aws-s3-access-id
-merge_secret backup-s3-cfg aws-s3-access-key
-
+kubectl create secret generic backup-s3-secret  --dry-run=client --from-file=S3_BUCKET_NAME=./secrets/s3-bucket-name -n postgres  -o yaml | kubeseal --cert ../K8s-cluster/sealed-secrets/sealed-secrets-pub.pem  --format yaml --merge-into sealed-secrets/backup-s3-secret.yaml
