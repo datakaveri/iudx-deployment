e7ec0a279d305e39ca64e843fad9cb617c4b7de4
abhilash <abhilashnitk3@gmail.com>
2021-09-05 16:22:56 +0000

    Adding items:
    - es-autoscaler, es-exporter
    - kibana
    - logstash

--- K8s-deployment/resource-server/database/keystore-generators/elasticsearch.sh
+++ K8s-deployment/resource-server/database/keystore-generators/elasticsearch.sh
@@ -7,7 +7,7 @@ keys="./secrets/pki"
 elasticsearch-keystore create --silent
 
 cat "$passwords/elasticsearch-su-password" | elasticsearch-keystore add bootstrap.password
-#cat "$keys/s3-access-key" | elasticsearch-keystore add s3.client.default.access_key
-#cat "$keys/s3-secret-key" | elasticsearch-keystore add s3.client.default.secret_key
+cat "$keys/s3-access-key" | elasticsearch-keystore add s3.client.default.access_key
+cat "$keys/s3-secret-key" | elasticsearch-keystore add s3.client.default.secret_key
 
 cp /usr/share/elasticsearch/config/elasticsearch.keystore "$keystore"
