38ad2a1c66768ba3b8e886426e84959e500274de
abhilash <abhilashnitk3@gmail.com>
2021-09-05 13:24:53 +0000

    Add redis-autoscaler and some fine tunings

--- K8s-deployment/redis/install.sh
+++ K8s-deployment/redis/install.sh
@@ -7,3 +7,8 @@ sleep 10
 # install redis cluster  
 helm install -f redis-values.yaml -f resource-values.yaml redis  --version 6.3.3 $@ bitnami/redis-cluster -n redis
 
+sleep 50
+#redis-autoscaler
+kubectl apply -f autoscale-rbac.yaml
+kubectl create configmap redis-autoscale-script   -n redis --from-file=./redis-autoscaler.sh
+kubectl apply -f autoscale-cron.yml 
