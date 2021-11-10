97d30cf1157d54446c837e47ed6d3ca6520c8e33
Abhilash V <abhilashnitk3@gmail.com>
2021-11-10 18:48:36 +0530

    Changes to  catalogue K8s orchestration

--- K8s-deployment/catalogue/apiserver/delete.sh
+++ K8s-deployment/catalogue/apiserver/delete.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 kubectl delete -f cat-deployment/
-kubectl delete secret cat-config-apiserver -n cat-test
-kubectl delete secret cat-config-database -n cat-test
-kubectl delete secret cat-config-authenticator -n cat-test
-kubectl delete secret cat-config-validator -n cat-test
-kubectl delete secret  cat-keystore -n cat-test
-kubectl delete configmap cat-docs  -n cat-test
-kubectl delete configmap cat-env -n cat-test
-kubectl delete -f zookeeper.yaml
+kubectl delete secret cat-config-apiserver -n cat
+kubectl delete secret cat-config-database -n cat
+kubectl delete secret cat-config-authenticator -n cat
+kubectl delete secret cat-config-validator -n cat
+kubectl delete secret cat-config-auditing -n cat
+kubectl delete secret  cat-keystore -n cat
+kubectl delete configmap cat-env -n cat
