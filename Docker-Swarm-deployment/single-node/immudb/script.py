c9ec26e96638261a95c5289e6f38c7efa0c26ad2
namanmanish <namanmanishtrivedi@gmail.com>
2021-11-17 16:26:17 +0530

    Added stack file for the config-helper and fixed a few bugs

--- Docker-Swarm-deployment/single-node/immudb/script.py
+++ Docker-Swarm-deployment/single-node/immudb/script.py
@@ -10,14 +10,8 @@ IUDX_AUTH_PASSWORD = f.read()
 f = open("../run/secrets/catpassword","r")
 IUDX_CAT_PASSWORD = f.read()
 
-f = open("../run/secrets/defaultpassword","r")
-IUDX_DEFAULT_PASSWORD = f.read()
-
 f = open("../run/secrets/rspassword","r")
 IUDX_RS_PASSWORD = f.read()
-
-f = open("../run/secrets/testpassword","r")
-IUDX_TEST_PASSWORD = f.read()
 
 client = ImmudbClient("immudb:3322")
 client.login("immudb","immudb")
@@ -27,14 +21,11 @@ client.login("immudb",ADMIN_PASSWORD)
 
 client.databaseCreate("iudxauth")
 client.databaseCreate("iudxcat")
-client.databaseCreate("iudxrsorg")
 client.databaseCreate("iudxrs")
 
 client.createUser("iudx_auth",IUDX_AUTH_PASSWORD,immudb.constants.PERMISSION_RW,"iudxauth")
 client.createUser("iudx_cat",IUDX_CAT_PASSWORD,immudb.constants.PERMISSION_RW,"iudxcat")
-client.createUser("iudx_default",IUDX_DEFAULT_PASSWORD,immudb.constants.PERMISSION_RW,"defaultdb")
-client.createUser("iudx_rs",IUDX_RS_PASSWORD,immudb.constants.PERMISSION_RW,"iudxrsorg")
-client.createUser("test",IUDX_TEST_PASSWORD,immudb.constants.PERMISSION_RW,"iudxrs")
+client.createUser("iudx_rs",IUDX_RS_PASSWORD,immudb.constants.PERMISSION_RW,"iudxrs")
 
 client.databaseUse("iudxcat")
 client.sqlExec("CREATE TABLE auditingtable(id VARCHAR NOT NULL, userRole VARCHAR NOT NULL,userID VARCHAR NOT NULL,iid VARCHAR NOT NULL,api VARCHAR NOT NULL,method VARCHAR NOT NULL,time INTEGER NOT NULL,iudxID VARCHAR NOT NULL,PRIMARY KEY id);")
@@ -44,3 +35,18 @@ client.sqlExec("CREATE TABLE table_auditing(id VARCHAR NOT NULL, body VARCHAR NO
 
 client.databaseUse("iudxrs")
 client.sqlExec("CREATE TABLE auditing(id VARCHAR NOT NULL, userid VARCHAR NOT NULL,api VARCHAR NOT NULL,resourceid VARCHAR NOT NULL,time INTEGER NOT NULL,PRIMARY KEY id);")
+
+print(client.listUsers())
+print(client.databaseList())
+
+client.databaseUse("iudxcat")
+print(client.listTables())
+# print(client.describeTable("auditingtable"))
+
+client.databaseUse("iudxauth")
+print(client.listTables())
+# print(client.describeTable("table_auditing"))
+
+client.databaseUse("iudxrs")
+print(client.listTables())
+# print(client.describeTable("auditing"))
