0094b018c09aa1352fab5c2065f2405143af1abb
Abhilash V <abhilashnitk3@gmail.com>
2021-12-24 17:25:19 +0530

    removing previous catalogue

--- K8s-deployment/catalogue/apiserver/delete.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-kubectl delete -f cat-deployment/
-kubectl delete secret cat-config-apiserver -n cat
-kubectl delete secret cat-config-database -n cat
-kubectl delete secret cat-config-authenticator -n cat
-kubectl delete secret cat-config-validator -n cat
-kubectl delete secret cat-config-auditing -n cat
-kubectl delete secret  cat-keystore -n cat
-kubectl delete configmap cat-env -n cat
