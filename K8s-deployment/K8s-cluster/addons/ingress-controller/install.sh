3d16126c2efa4ddb813b969f4de52a0ee88608b4
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-02-17 22:24:12 +0530

    Adding ingress-controller addon

---
+++ K8s-deployment/K8s-cluster/addons/ingress-controller/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+kubectl create namespace ingress-nginx
+kubectl create configmap  error-conf --from-file=./ingress-nginx/conf/error.conf -n ingress-nginx 
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx && helm repo add bitnami https://charts.bitnami.com/bitnami &&  helm repo update
+helm install --version=4.0.1 nginx ingress-nginx/ingress-nginx  -f ingress-nginx/nginx-values.yaml -f ingress-nginx/resource-values.yaml -n ingress-nginx
+helm install memcached bitnami/memcached --version=5.14.2 -f memcached/memcached-values.yaml -f memcached/resource-values.yaml -n memcached
+
