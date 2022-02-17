fccb0a0bb7ddaf42187783a772e35041658b4707
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-02-17 22:52:46 +0530

    Bugs fix ingress, aws nlb related issue
    - bug-fix nlb related issue in cloud init scripts
    - delete unwanted file
    - add some corrections to ingress-controller

--- K8s-deployment/K8s-cluster/Rancher/configs/aws/K8s-worker-cloud-init.sh
+++ K8s-deployment/K8s-cluster/Rancher/configs/aws/K8s-worker-cloud-init.sh
@@ -15,7 +15,9 @@ sudo usermod -aG docker ubuntu
 
 TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 PRIVATE_IP=$(curl -H "X-aws-ec2-metadata-token: ${TOKEN}" -s http://169.254.169.254/latest/meta-data/local-ipv4)
+# Add --node-name when setting up K8s cluster using Rancher in cloud init scripts, https://github.com/rancher/rancher/issues/22416#issuecomment-531249541
+NODE_NAME=$(curl -H "X-aws-ec2-metadata-token: ${TOKEN}" -s http://169.254.169.254/latest/meta-data/local-hostname)
 K8S_ROLES="--worker"
 
-sudo docker run -d --privileged --restart=unless-stopped --net=host -v /etc/kubernetes:/etc/kubernetes -v /var/run:/var/run  rancher/rancher-agent:v2.5.9 --server https://<rancher_server_url> --token <token_value> --ca-checksum <ca_checksum_value-optional>  --internal-address $PRIVATE_IP $K8S_ROLES  
+sudo docker run -d --privileged --restart=unless-stopped --net=host -v /etc/kubernetes:/etc/kubernetes -v /var/run:/var/run  rancher/rancher-agent:v2.5.9 --server https://<rancher_server_url> --token <token_value> --ca-checksum <ca_checksum_value-optional>  --internal-address $PRIVATE_IP $K8S_ROLES  --node-name $NODE_NAME
 
