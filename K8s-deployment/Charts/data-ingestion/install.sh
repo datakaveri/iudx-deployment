549d2865e2e4743e2e4a27b1584f9911a82595e0
karun-singh <karun.singh@datakaveri.org>
2022-03-23 16:26:16 +0530

    updated install script and README

--- K8s-deployment/Charts/data-ingestion/install.sh
+++ K8s-deployment/Charts/data-ingestion/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 kubectl create namespace di
+kubectl create configmap di-env --from-env-file=./secrets/.di.env -n di
 kubectl create secret generic di-config --from-file=configs/config.json -n di
-helm install di ../data-ingestion -f values.yaml -f resource-values.yaml -n di
+helm install data-ingestion ../data-ingestion -f values.yaml -f resource-values.yaml -n di
