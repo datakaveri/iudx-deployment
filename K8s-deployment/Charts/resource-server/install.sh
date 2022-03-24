e1146747aa922171b061e48e34779d0f5f58d1ed
karun-singh <66350877+karun-singh@users.noreply.github.com>
2022-03-24 16:36:07 +0530

    Fixed env file reference

--- K8s-deployment/Charts/resource-server/install.sh
+++ K8s-deployment/Charts/resource-server/install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 kubectl create namespace rs
-kubectl create configmap rs-env --from-env-file=./secrets/.rs-api.env -n rs
+kubectl create configmap rs-env --from-env-file=./secrets/.rs.env -n rs
 kubectl create secret generic rs-s3-env --from-file=./secrets/AWS_ACCESS_KEY_ID --from-file=./secrets/AWS_SECRET_ACCESS_KEY -n rs
 kubectl create secret generic rs-config --from-file=./configs/config.json -n rs
 helm install resource-server ../resource-server -f values.yaml -f resource-values.yaml $@ -n rs
