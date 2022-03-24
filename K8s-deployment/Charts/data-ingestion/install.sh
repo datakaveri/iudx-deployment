933c62f1d0ea292b6e839bc9b20d3dadde81837e
karun-singh <66350877+karun-singh@users.noreply.github.com>
2022-03-24 16:43:59 +0530

    fixed config file path

--- K8s-deployment/Charts/data-ingestion/install.sh
+++ K8s-deployment/Charts/data-ingestion/install.sh
@@ -2,5 +2,5 @@
 
 kubectl create namespace di
 kubectl create configmap di-env --from-env-file=./secrets/.di.env -n di
-kubectl create secret generic di-config --from-file=configs/config.json -n di
+kubectl create secret generic di-config --from-file=./secrets/config.json -n di
 helm install data-ingestion ../data-ingestion -f values.yaml -f resource-values.yaml $@ -n di
