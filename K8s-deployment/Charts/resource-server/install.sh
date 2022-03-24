f604a792e6d27f8e9dcd14fd14b2551ff4397b8b
karun-singh <66350877+karun-singh@users.noreply.github.com>
2022-03-24 16:37:39 +0530

    fixed config file path

--- K8s-deployment/Charts/resource-server/install.sh
+++ K8s-deployment/Charts/resource-server/install.sh
@@ -3,5 +3,5 @@
 kubectl create namespace rs
 kubectl create configmap rs-env --from-env-file=./secrets/.rs.env -n rs
 kubectl create secret generic rs-s3-env --from-file=./secrets/AWS_ACCESS_KEY_ID --from-file=./secrets/AWS_SECRET_ACCESS_KEY -n rs
-kubectl create secret generic rs-config --from-file=./configs/config.json -n rs
+kubectl create secret generic rs-config --from-file=./secrets/config.json -n rs
 helm install resource-server ../resource-server -f values.yaml -f resource-values.yaml $@ -n rs
