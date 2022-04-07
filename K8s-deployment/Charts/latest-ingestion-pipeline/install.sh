5d80c18d06ba6c5820f87fa1bd187d7e10174958
karun-singh <karun.singh@datakaveri.org>
2022-04-07 11:32:58 +0530

    removed attribute mapping secret

--- K8s-deployment/Charts/latest-ingestion-pipeline/install.sh
+++ K8s-deployment/Charts/latest-ingestion-pipeline/install.sh
@@ -2,6 +2,5 @@
 
 kubectl create namespace lip
 kubectl create configmap lip-env --from-env-file=./secrets/.lip.env -n lip
-kubectl create secret generic attribute-mapping --from-file=./secrets/attribute-mapping.json -n lip
 kubectl create secret generic lip-config --from-file=./secrets/config.json -n lip
 helm install latest-ingestion-pipeline ../latest-ingestion-pipeline -f values.yaml -f resource-values.yaml $@ -n lip
