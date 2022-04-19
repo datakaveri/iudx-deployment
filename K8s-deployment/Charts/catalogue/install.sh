ceaa5b52b7198f9bf1c8c555b23383bec7543b05
karun-singh <karun.singh@datakaveri.org>
2022-04-19 15:38:27 +0530

    fixed targetPort in values yaml and directory name in install.sh

--- K8s-deployment/Charts/catalogue/install.sh
+++ K8s-deployment/Charts/catalogue/install.sh
@@ -3,4 +3,4 @@
 kubectl create namespace cat
 kubectl create configmap cat-env --from-env-file=./secrets/.cat.env -n cat
 kubectl create secret generic cat-config --from-file=./secrets/config.json -n cat
-helm install catalogue-server ../catalogue-server -f values.yaml -f resource-values.yaml $@ -n cat
+helm install catalogue-server ../catalogue -f values.yaml -f resource-values.yaml $@ -n cat
