820b8814eb1eaf0cb14ac3db298e4485e50dc58b
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-05-16 16:16:33 +0530

    changes for k8s redis
    - remove sealed secrets -> now
    - add example-aws-resources
    - rename yml to yaml files
    - Update Readme

--- K8s-deployment/redis/install.sh
+++ K8s-deployment/redis/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 kubectl create namespace redis
-kubectl apply -f ../K8s-cluster/sealed-secrets/cluster-wide-sealed-secrets/docker-registry-cred.yaml -n redis
-kubectl apply -f sealed-secrets/
+kubectl create secret generic redis-passwords  --from-file=./secrets/redis-password -n redis  -o yaml 
 sleep 10
 # install redis cluster  
 helm install -f redis-values.yaml -f resource-values.yaml redis  --version 6.3.3 $@ bitnami/redis-cluster -n redis
@@ -11,4 +10,4 @@ sleep 50
 #redis-autoscaler
 kubectl apply -f autoscale-rbac.yaml
 kubectl create configmap redis-autoscale-script   -n redis --from-file=./redis-autoscaler.sh
-kubectl apply -f autoscale-cron.yml 
+kubectl apply -f autoscale-cron.yaml 
