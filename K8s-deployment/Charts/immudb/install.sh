0d2b85ef11298556a3c0eb3285fd45e7a9d02d8d
Pranav Pathak <pathakpranav90@gmail.com>
2022-05-26 12:33:28 +0530

    updates install script  and hook-image

--- K8s-deployment/Charts/immudb/install.sh
+++ K8s-deployment/Charts/immudb/install.sh
@@ -1,4 +1,4 @@
 kubectl create namespace immudb
-kubectl create secret generic immudb-admin-password --from-file=../secrets/immudb/immudb-admin-password -n immudb
-kubectl create secret generic hook-secret --from-file=../secrets/immudb/password -n immudb
+kubectl create secret generic immudb-admin-password --from-file=secrets/immudb-admin-password -n immudb
+kubectl create secret generic hook-secret --from-file=secrets/password -n immudb
 helm install immudb . -f values.yaml -f resource-values.yaml  -n immudb
