6b5254b2edc6611cfa9f4759385bdee66af7ee95
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-06-06 11:22:38 +0530

    Adding rmq changes

--- K8s-deployment/resource-server/databroker/delete-dependencies.sh
+++ K8s-deployment/resource-server/databroker/delete-dependencies.sh
@@ -1,5 +1,6 @@
 kubectl delete -f internal-client-service.yaml
 kubectl delete -f external-client-service.yaml
+kubectl delete -f rabbitmq-logstash-client-service.yaml
 kubectl delete -f headless-service.yaml
 kubectl delete -f configmap.yaml
 kubectl delete -f rbac.yaml
