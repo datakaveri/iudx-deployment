4820509eb5bb285135201fef070359053acf54cf
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-06-06 12:02:59 +0530

    Adding keycloak changes
    - adding certmanager annotation for certs
    - remove sealed secrets

--- K8s-deployment/keycloak/install.sh
+++ K8s-deployment/keycloak/install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 kubectl create namespace keycloak
-kubectl apply -f ../K8s-cluster/sealed-secrets/cluster-wide-sealed-secrets/docker-registry-cred.yaml -n keycloak
-kubectl apply -f sealed-secrets/
+kubectl create secret generic keycloak-passwords  --from-file=./secrets/admin-password --fom-file=./secrets/management-password -n keycloak
+kubectl create secret generic keycloak-env-secret   --from-file=KEYCLOAK_DATABASE_PASSWORD=./secrets/database-password -n keycloak                  
+
+kubectl create secret generic keycloak-env-secret  --from-file=KEYCLOAK_ADMIN_USER=./secrets/admin-username -n keycloak          
+
+kubectl create secret generic keycloak-env-secret  --from-file=KEYCLOAK_MANAGEMENT_USER=./secrets/management-username -n keycloak
 sleep 10
 # install keycloak cluster  
 helm install -f keycloak-values.yaml -f resource-values.yaml keycloak  --version 4.0.1 $@ bitnami/keycloak -n keycloak
