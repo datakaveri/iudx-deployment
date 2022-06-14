85e8e94c5eeec7d828b46f58efc166371c1967fc
karun-singh <karun.singh@datakaveri.org>
2022-06-14 12:54:56 +0530

    added install scripts, and sample secrets and resource-values.yaml

--- K8s-deployment/Charts/immudb/install.sh
+++ K8s-deployment/Charts/immudb/install.sh
@@ -1,4 +1,7 @@
-kubectl create namespace immudb
+#!/bin/bash
+
+kubectl create ns immudb
 kubectl create secret generic immudb-admin-password --from-file=secrets/immudb-admin-password -n immudb
-kubectl create secret generic hook-secret --from-file=secrets/password -n immudb
-helm install immudb . -f values.yaml -f resource-values.yaml  -n immudb
+kubectl create secret generic hook-secret --from-file=./secrets/passwords -n immudb
+
+helm install immudb ../immudb -f values.yaml -f resource-values.yaml -n immudb
