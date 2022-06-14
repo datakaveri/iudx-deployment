6e546c2595fd42c7a8b615d46c831f49edac50aa
karun-singh <karun.singh@datakaveri.org>
2022-06-14 19:43:04 +0530

    updated keycloak install script

--- K8s-deployment/Charts/keycloak/install.sh
+++ K8s-deployment/Charts/keycloak/install.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 kubectl create namespace keycloak
-kubectl create secret generic keycloak-passwords  --from-file=./secrets/admin-password --fom-file=./secrets/management-password -n keycloak
-kubectl create secret generic keycloak-env-secret   --from-file=KEYCLOAK_DATABASE_PASSWORD=./secrets/database-password -n keycloak                  
+kubectl create secret generic keycloak-passwords  --from-file=./secrets/admin-password --from-file=./secrets/management-password -n keycloak
+kubectl create secret generic keycloak-env-secret   --from-file=KEYCLOAK_DATABASE_PASSWORD=./secrets/database-password --from-file=KEYCLOAK_ADMIN_USER=./secrets/admin-username --from-file=KEYCLOAK_MANAGEMENT_USER=./secrets/management-username -n keycloak
 
-kubectl create secret generic keycloak-env-secret  --from-file=KEYCLOAK_ADMIN_USER=./secrets/admin-username -n keycloak          
-
-kubectl create secret generic keycloak-env-secret  --from-file=KEYCLOAK_MANAGEMENT_USER=./secrets/management-username -n keycloak
 sleep 10
 # install keycloak cluster  
+helm repo add bitnami https://raw.githubusercontent.com/bitnami/charts/43174e1970616584f893f66c1fbcac00d110c633/bitnami/
 helm install -f keycloak-values.yaml -f resource-values.yaml keycloak  --version 4.0.1 $@ bitnami/keycloak -n keycloak
-
