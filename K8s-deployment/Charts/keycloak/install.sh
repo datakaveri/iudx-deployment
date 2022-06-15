3728637c23ad283b66d8966289d6cdea2d99be8d
karun-singh <karun.singh@datakaveri.org>
2022-06-15 13:35:33 +0530

    created sample resource value yamls for aws and azure, renamed values.yaml

--- K8s-deployment/Charts/keycloak/install.sh
+++ K8s-deployment/Charts/keycloak/install.sh
@@ -7,4 +7,4 @@ kubectl create secret generic keycloak-env-secret   --from-file=KEYCLOAK_DATABAS
 sleep 10
 # install keycloak cluster  
 helm repo add bitnami https://raw.githubusercontent.com/bitnami/charts/43174e1970616584f893f66c1fbcac00d110c633/bitnami/
-helm install -f keycloak-values.yaml -f resource-values.yaml keycloak  --version 4.0.1 $@ bitnami/keycloak -n keycloak
+helm install -f values.yaml -f resource-values.yaml keycloak  --version 4.0.1 $@ bitnami/keycloak -n keycloak
