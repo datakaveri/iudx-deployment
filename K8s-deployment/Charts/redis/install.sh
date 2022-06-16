b9eedbfc3063b700447f9693ddea0f02dae32162
karun-singh <karun.singh@datakaveri.org>
2022-06-16 19:11:16 +0530

    renamed redis-values.yaml to values.yaml

--- K8s-deployment/Charts/redis/install.sh
+++ K8s-deployment/Charts/redis/install.sh
@@ -5,10 +5,10 @@ kubectl create secret generic redis-passwords --from-file=./secrets/redis-passwo
 sleep 10
 # install redis cluster  
 helm repo add bitnami https://raw.githubusercontent.com/bitnami/charts/43174e1970616584f893f66c1fbcac00d110c633/bitnami/
-helm install -f redis-values.yaml -f resource-values.yaml redis  --version 6.3.3 $@ bitnami/redis-cluster -n redis
+helm install -f values.yaml -f resource-values.yaml redis  --version 6.3.3 $@ bitnami/redis-cluster -n redis
 
 sleep 50
 #redis-autoscaler
 kubectl apply -f autoscale-rbac.yaml
-kubectl create configmap redis-autoscale-script   -n redis --from-file=./redis-autoscaler.sh
+kubectl create configmap redis-autoscale-script --from-file=./redis-autoscaler.sh -n redis
 kubectl apply -f autoscale-cron.yaml 
