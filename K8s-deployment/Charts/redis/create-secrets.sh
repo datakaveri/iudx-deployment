b82d2beb2ee99780d2dfe639cae3062e4c3da461
karun-singh <karun.singh@datakaveri.org>
2022-06-17 12:51:09 +0530

    updated secret generation script to only use alphanumeric characters

--- K8s-deployment/Charts/redis/create-secrets.sh
+++ K8s-deployment/Charts/redis/create-secrets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir ./secrets/
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/redis-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 16) > secrets/redis-password
