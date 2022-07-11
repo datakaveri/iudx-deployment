0dd297eda24615d62116ae7024349d6551ea4718
Abhi <29863835+abhi4578@users.noreply.github.com>
2022-07-11 11:58:26 +0530

    Add memcached ns

--- K8s-deployment/K8s-cluster/addons/ingress-controller/install.sh
+++ K8s-deployment/K8s-cluster/addons/ingress-controller/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl create namespace ingress-nginx
+kubectl create namespace ingress-nginx && kubectl create namespace memcached
 kubectl create configmap  error-conf --from-file=./ingress-nginx/conf/error.conf -n ingress-nginx 
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx && helm repo add bitnami https://raw.githubusercontent.com/bitnami/charts/43174e1970616584f893f66c1fbcac00d110c633/bitnami/ &&  helm repo update
 helm install --version=4.0.1 nginx ingress-nginx/ingress-nginx  -f ingress-nginx/nginx-values.yaml -f ingress-nginx/resource-values.yaml -n ingress-nginx && sleep 10 
