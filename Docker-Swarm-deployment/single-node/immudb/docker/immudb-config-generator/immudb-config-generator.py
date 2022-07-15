ba0eafa8c9f15e7a45a1a1aad2ce9ec1c98fe09f
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-07-15 15:30:52 +0530

    Removing rsauditing table,create index for cat,aaa

--- Docker-Swarm-deployment/single-node/immudb/docker/immudb-config-generator/immudb-config-generator.py
+++ Docker-Swarm-deployment/single-node/immudb/docker/immudb-config-generator/immudb-config-generator.py
@@ -38,15 +38,13 @@ client.createUser("iudx_cat",IUDX_CAT_PASSWORD,immudb.constants.PERMISSION_RW,"i
 client.createUser("iudx_rs",IUDX_RS_PASSWORD,immudb.constants.PERMISSION_RW,"iudxrsorg")
 
 client.useDatabase("iudxcat")
-client.sqlExec("CREATE TABLE auditingtable(id VARCHAR[250] NOT NULL, userRole VARCHAR[250] NOT NULL,userID VARCHAR[250] NOT NULL,iid VARCHAR[250] NOT NULL,api VARCHAR[250] NOT NULL,method VARCHAR[250] NOT NULL,time INTEGER NOT NULL,iudxID VARCHAR[250] NOT NULL,PRIMARY KEY id);")
-
+client.sqlExec("CREATE TABLE auditingtable(id VARCHAR[128] NOT NULL, userRole VARCHAR[250] NOT NULL,userID VARCHAR[250] NOT NULL,iid VARCHAR[250] NOT NULL,api VARCHAR[250] NOT NULL,method VARCHAR[250] NOT NULL,time INTEGER NOT NULL,iudxID VARCHAR[250] NOT NULL,PRIMARY KEY id);")
+client.sqlExec("CREATE INDEX ON auditingtable(userID, iudxID, time, api);")
 client.useDatabase("iudxauth")
 client.sqlExec("CREATE TABLE table_auditing(id VARCHAR[250] NOT NULL, body VARCHAR[250] NOT NULL,userid VARCHAR[250] NOT NULL,endpoint VARCHAR[250] NOT NULL,method VARCHAR[250] NOT NULL,time INTEGER NOT NULL,PRIMARY KEY id);")
-
+client.sqlExec("CREATE INDEX ON table_auditing(userid, endpoint, time);")
 client.useDatabase("iudxrsorg")
-client.sqlExec("CREATE TABLE IF NOT EXISTS rsauditingtable (id VARCHAR[128] NOT NULL, api VARCHAR[128], userid VARCHAR[128],epochtime INTEGER,resourceid VARCHAR[256], isotime VARCHAR[64], providerid VARCHAR[128], PRIMARY KEY id);")
 
-client.sqlExec("CREATE INDEX ON rsauditingtable(userid, epochtime, providerid, resourceid);")
 
 client.sqlExec("CREATE TABLE rsaudit (id VARCHAR[128] NOT NULL,api VARCHAR[128] NOT NULL,userid VARCHAR[128] NOT NULL,epochtime INTEGER NOT NULL,resourceid VARCHAR[256] NOT NULL,isotime VARCHAR[64] NOT NULL,providerid VARCHAR[128] NOT NULL,size INTEGER, PRIMARY KEY id);")
 client.sqlExec("CREATE INDEX ON rsaudit(userid, epochtime, providerid, resourceid);")
