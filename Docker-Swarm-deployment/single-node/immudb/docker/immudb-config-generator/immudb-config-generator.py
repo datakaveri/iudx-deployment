028570d26aa90b17cd5e4f2fba0e968eb21e9fe0
karun-singh <karun.singh@datakaveri.org>
2022-07-15 13:59:31 +0530

    updated immudb table schema

--- Docker-Swarm-deployment/single-node/immudb/docker/immudb-config-generator/immudb-config-generator.py
+++ Docker-Swarm-deployment/single-node/immudb/docker/immudb-config-generator/immudb-config-generator.py
@@ -36,13 +36,13 @@ client.createUser("iudx_cat",IUDX_CAT_PASSWORD,immudb.constants.PERMISSION_RW,"i
 client.createUser("iudx_rs",IUDX_RS_PASSWORD,immudb.constants.PERMISSION_RW,"iudxrs")
 
 client.databaseUse("iudxcat")
-client.sqlExec("CREATE TABLE auditingtable(id VARCHAR NOT NULL, userRole VARCHAR NOT NULL,userID VARCHAR NOT NULL,iid VARCHAR NOT NULL,api VARCHAR NOT NULL,method VARCHAR NOT NULL,time INTEGER NOT NULL,iudxID VARCHAR NOT NULL,PRIMARY KEY id);")
+client.sqlExec("CREATE TABLE auditingtable(id VARCHAR[128] NOT NULL, userRole VARCHAR[128] NOT NULL,userID VARCHAR[128] NOT NULL,iid VARCHAR[200] NOT NULL,api VARCHAR[128] NOT NULL,method VARCHAR[128] NOT NULL,time INTEGER NOT NULL,iudxID VARCHAR[200] NOT NULL,PRIMARY KEY id);")
 
 client.databaseUse("iudxauth")
-client.sqlExec("CREATE TABLE table_auditing(id VARCHAR NOT NULL, body VARCHAR NOT NULL,userid VARCHAR NOT NULL,endpoint VARCHAR NOT NULL,method VARCHAR NOT NULL,time INTEGER NOT NULL,PRIMARY KEY id);")
+client.sqlExec("CREATE TABLE table_auditing(id VARCHAR[128] NOT NULL, body VARCHAR[200] NOT NULL,userid VARCHAR[128] NOT NULL,endpoint VARCHAR[128] NOT NULL,method VARCHAR[128] NOT NULL,time INTEGER NOT NULL,PRIMARY KEY id);")
 
 client.databaseUse("iudxrs")
-client.sqlExec("CREATE TABLE auditing(id VARCHAR NOT NULL, userid VARCHAR NOT NULL,api VARCHAR NOT NULL,resourceid VARCHAR NOT NULL,time INTEGER NOT NULL,PRIMARY KEY id);")
+client.sqlExec("CREATE TABLE rsauditingtable (id VARCHAR[128] NOT NULL, api VARCHAR[128],userid VARCHAR[128],epochtime INTEGER,resourceid VARCHAR[200],isotime VARCHAR[128],providerid VARCHAR[128],size INTEGER,PRIMARY KEY id);")
 
 print(client.listUsers())
 print(client.databaseList())
