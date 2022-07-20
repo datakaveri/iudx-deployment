4ee26dd180eaa24d2ec72397a3552aaf2edc7038
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-07-20 11:14:15 +0530

    corrections in postgresql
    - use of helm delete and helm install. Because Helm upgrade to psql
      may cause inconsistent/partitioned cluster formation
    - add detailed flyway instructions in Readme
    - add ./secrets/passwords/.gitkeep

--- K8s-deployment/Charts/postgresql/install.sh
+++ K8s-deployment/Charts/postgresql/install.sh
@@ -9,10 +9,13 @@ sleep 10
 # install postgres asynchronous cluster  
 helm repo add bitnami https://raw.githubusercontent.com/bitnami/charts/43174e1970616584f893f66c1fbcac00d110c633/bitnami/
 helm install -f psql-async-values.yaml -f resource-values.yaml psql --version 7.7.3 $@ bitnami/postgresql-ha -n postgres
-sleep 100
 
-# helm upgrade to postgres quorum synchronous replication cluster
+# helm delete and install  to postgres quorum synchronous replication cluster
 POSTGRES_PASSWORD=$(kubectl get secret --namespace  postgres psql-passwords -o jsonpath="{.data.postgresql-password}" | base64 --decode)
 REPMGR_PASSWORD=$(kubectl get secret --namespace postgres psql-passwords -o jsonpath="{.data.repmgr-password}" | base64 --decode)
 POOL_ADMIN_PASSWORD=$(kubectl get secret --namespace postgres psql-postgresql-ha-pgpool -o jsonpath="{.data.admin-password}" | base64 --decode)
-helm upgrade -f psql-async-values.yaml -f psql-sync-values.yaml -f resource-values.yaml -n  postgres psql  bitnami/postgresql-ha --version=7.7.3 $@ --set postgresql.password=$POSTGRES_PASSWORD  --set postgresql.repmgrPassword=$REPMGR_PASSWORD --set pgpool.adminPassword=$POOL_ADMIN_PASSWORD  
+sleep 150
+
+helm delete -n postgres  psql && sleep 50
+
+helm install -f psql-async-values.yaml -f psql-sync-values.yaml -f resource-values.yaml -n  postgres psql  bitnami/postgresql-ha --version=7.7.3 $@ --set postgresql.password=$POSTGRES_PASSWORD  --set postgresql.repmgrPassword=$REPMGR_PASSWORD --set pgpool.adminPassword=$POOL_ADMIN_PASSWORD  
