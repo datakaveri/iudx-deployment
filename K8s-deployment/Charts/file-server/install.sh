dd6d4e3a1aadbaeac1e6576433423d45f4189bae
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-07-22 13:44:29 +0530

    correct resource-values in gis, file

--- K8s-deployment/Charts/file-server/install.sh
+++ K8s-deployment/Charts/file-server/install.sh
@@ -1,4 +1,4 @@
 kubectl create namespace fs
 kubectl create configmap fs-env --from-env-file=secrets/.fs.env -n fs
 kubectl create secret generic fs-config --from-file=secrets/config.json -n fs
-helm install file-server -f resource-values.yaml -f values.yaml ../file-server/ $@ -n fs
+helm install file-server  -f values.yaml -f resource-values.yaml ../file-server/ $@ -n fs
