94d278fa12e55f5519373c225c889f20240b8004
karun-singh <karun.singh@datakaveri.org>
2022-08-08 17:45:24 +0530

    changed to bitnami full index repo

--- K8s-deployment/Charts/redis/install.sh
+++ K8s-deployment/Charts/redis/install.sh
@@ -4,7 +4,7 @@ kubectl create namespace redis
 kubectl create secret generic redis-passwords --from-file=./secrets/redis-password -n redis
 sleep 10
 # install redis cluster  
-helm repo add bitnami https://raw.githubusercontent.com/bitnami/charts/61df8c90d68aa4598401c27a9057baaf30a44935/bitnami/index.yaml
+helm repo add bitnami https://raw.githubusercontent.com/bitnami/charts/archive-full-index/bitnami
 helm install -f values.yaml -f resource-values.yaml redis  --version 8.0.0 $@ bitnami/redis-cluster -n redis
 
 sleep 50
