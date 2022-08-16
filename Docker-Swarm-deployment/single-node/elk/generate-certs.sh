a1866c5303ae92e719196a3ad28099b419c6ce73
Pranav Pathak <pathakpranav90@gmail.com>
2022-08-17 01:13:54 +0530

    ELK Version to 8.3.3

---
+++ Docker-Swarm-deployment/single-node/elk/generate-certs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+docker run  --rm  -it  -v $(pwd)/secrets:/tmp/secrets \
+  -u 1000  docker.elastic.co/elasticsearch/elasticsearch:8.3.3\
+		/bin/sh -c " \
+			elasticsearch-certutil ca --out /tmp/secrets/pki/elastic-stack-ca.p12 --pass '' && \
+			elasticsearch-certutil cert --name security-master  --ca /tmp/secrets/pki/elastic-stack-ca.p12 --pass '' --ca-pass '' --out /tmp/secrets/pki/elastic-certificates.p12" 
+
--- Docker-Swarm-deployment/single-node/elk/generate-keystore.sh
+++ Docker-Swarm-deployment/single-node/elk/generate-keystore.sh
@@ -7,18 +7,19 @@ docker run \
     --rm -it \
     -v $(pwd)/keystore-generators/elasticsearch.sh:/usr/share/elasticsearch/run.sh \
     -v $(pwd)/secrets:/usr/share/elasticsearch/secrets \
-    docker.elastic.co/elasticsearch/elasticsearch:7.8.1 ./run.sh
+    docker.elastic.co/elasticsearch/elasticsearch:8.3.3 ./run.sh
 
 docker run \
     --rm -it \
     -v $(pwd)/keystore-generators/kibana.sh:/usr/share/kibana/run.sh \
     -v $(pwd)/secrets:/usr/share/kibana/secrets \
-    docker.elastic.co/kibana/kibana:7.8.1 ./run.sh
+    docker.elastic.co/kibana/kibana:8.3.3 ./run.sh
 
 docker run \
     --rm -it \
     -v $(pwd)/keystore-generators/logstash.sh:/usr/share/logstash/run.sh \
     -v $(pwd)/secrets:/usr/share/logstash/secrets \
-    docker.elastic.co/logstash/logstash:7.8.1 ./run.sh
+    docker.elastic.co/logstash/logstash:8.3.3 ./run.sh
 
 sudo chown -R 1000:0 secrets/keystores
+
