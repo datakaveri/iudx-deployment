026ad9ea02b1db5e3d298ae98c547ac11a7082ec
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-09-09 15:23:21 +0530

    keycloak corrections
    - rollback keycloak from 18.0.2 to 18.0.1
    due to issue [jdbc driver
    issue](https://github.com/bitnami/containers/issues/983)
    - Remove special symbol '@' in creation of postgres secrets,
    keycloak faced probelms in connecting when password had this

--- Docker-Swarm-deployment/single-node/postgres/create-secrets.sh
+++ Docker-Swarm-deployment/single-node/postgres/create-secrets.sh
@@ -4,8 +4,8 @@
 
 # create required secrets
 mkdir -p secrets/passwords
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgresql-password
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgres-auth-password
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgres-rs-password
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgres-keycloak-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgresql-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgres-auth-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgres-rs-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgres-keycloak-password
 
