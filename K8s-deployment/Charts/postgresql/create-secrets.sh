c43ac8ded7b67c44b37348d9a9a08ba131c7ef48
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-09-09 15:49:05 +0530

    corrections K8s postgres
    - Remove special symbol '@' in creation of postgres secrets,
      keycloak faced probelms in connecting when password had this

--- K8s-deployment/Charts/postgresql/create-secrets.sh
+++ K8s-deployment/Charts/postgresql/create-secrets.sh
@@ -3,11 +3,11 @@
 #set -e
 
 # create required secrets
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgresql-password
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/passwords/repmgr-password
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgres-auth-password
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgres-rs-password
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgres-keycloak-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgresql-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!#$%^&*+|:<>?' | head -c 16) > secrets/passwords/repmgr-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgres-auth-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgres-rs-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!#$%^&*+|:<>?' | head -c 16) > secrets/passwords/postgres-keycloak-password
 echo -n "postgres;iudx_rs_user;iudx_auth_user;iudx_keycloak_user" > secrets/passwords/usernames
 echo -n "$(cat secrets/passwords/postgresql-password);$(cat secrets/passwords/postgres-rs-password);$(cat secrets/passwords/postgres-auth-password);$(cat secrets/passwords/postgres-keycloak-password)" > secrets/passwords/passwords
 
