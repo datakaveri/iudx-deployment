363625184a9e9858f135feeb7bf06d88d5f0b660
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-09-16 17:34:12 +0530

    Update tests to include rate, conn limit

--- K8s-deployment/K8s-cluster/addons/ingress-controller/tests/connectivity-test.sh
+++ K8s-deployment/K8s-cluster/addons/ingress-controller/tests/connectivity-test.sh
@@ -2,11 +2,32 @@
 # test borrowed from tutorial https://kubernetes.io/docs/tasks/access-application-cluster/ingress-minikube/
 kubectl create deployment web --image=gcr.io/google-samples/hello-app:1.0
 kubectl expose deployment web --type=ClusterIP --port=8080
-kubectl apply -f https://k8s.io/examples/service/networking/example-ingress.yaml
+kubectl apply -f example-ingress-grl.yaml
 # map the loadabalncer ip to hello-world.info in your hosts file
-curl -vvv hello-world.info
+curl -vvv -k https://hello-world.info
 # output should be 
 # Hello, world!
 # Version: 1.0.0
 # Hostname: web-55b8c6998d-8k564
+
+# test global rate limiting
+ab -v 3 -n 200 -c 50 https://hello-world.info/  > /tmp/logs-grl.txt
+cat /tmp/logs-grl.txt | grep 429
+# atleast some requests will be rejected with 429 status cpde
+sleep 10
+
+# test rate limiting per ip
+kubectl apply -f example-ingress-rps.yaml
+ab -v 3 -n 200 -c 50 https://hello-world.info/  > /tmp/logs-rps.txt
+cat /tmp/logs-rps.txt | grep 429
+# atleast some requests will be rejected with 429 status cpde
+sleep 10
+
+# test connections limit
+kubectl apply -f example-ingress-connections.yaml
+ab -v 3 -n 200 -c 50 https://hello-world.info/  > /tmp/logs-conns.txt
+cat /tmp/logs-conns.txt | grep 503
+# atleast some requests will be rejected with 503 status cpde
+
+kubectl apply -f example-ingress-grl.yaml
 
