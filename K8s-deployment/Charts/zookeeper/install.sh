07cbd6eff43e5ecb3b79a11cb52e8fa0117a0564
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-09-21 00:09:21 +0530

    seperate ns creation command in zookeeper

--- K8s-deployment/Charts/zookeeper/install.sh
+++ K8s-deployment/Charts/zookeeper/install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 helm repo add bitnami https://raw.githubusercontent.com/bitnami/charts/archive-full-index/bitnami && helm repo update
-kubectl create namespace zookeeper &&  helm install -f zookeeper-values.yaml -f resource-values.yaml zookeeper --version 10.1.1  bitnami/zookeeper -n zookeeper
+kubectl create namespace zookeeper
+helm install -f zookeeper-values.yaml -f resource-values.yaml zookeeper --version 10.1.1  bitnami/zookeeper -n zookeeper
