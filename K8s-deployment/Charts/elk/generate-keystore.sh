cb6c966958bfea8b88a7b73674ae21db69a6df3d
karun-singh <karun.singh@datakaveri.org>
2022-09-26 07:23:28 +0000

    Elasticsearch 8.3.3 bitnami chart

--- K8s-deployment/Charts/elk/generate-keystore.sh
+++ K8s-deployment/Charts/elk/generate-keystore.sh
@@ -6,22 +6,15 @@ sudo chown -R 1000:0 secrets/keystores
 docker run \
     --rm -it \
     -u 1000:0 \
-    -v $(pwd)/keystore-generators/elasticsearch.sh:/usr/share/elasticsearch/run.sh \
-    -v $(pwd)/secrets:/usr/share/elasticsearch/secrets \
-    docker.elastic.co/elasticsearch/elasticsearch:7.12.1 ./run.sh
+    -v $(pwd)/keystore-generators/kibana.sh:/opt/bitnami/kibana/bin/run.sh \
+    -v $(pwd)/secrets:/opt/bitnami/kibana/secrets \
+    docker.io/bitnami/kibana:8.3.3-debian-11-r5 /opt/bitnami/kibana/bin/run.sh
 
 docker run \
     --rm -it \
     -u 1000:0 \
-    -v $(pwd)/keystore-generators/kibana.sh:/usr/share/kibana/run.sh \
-    -v $(pwd)/secrets:/usr/share/kibana/secrets \
-    docker.elastic.co/kibana/kibana:7.12.1 ./run.sh
-
-docker run \
-    --rm -it \
-    -u 1000:0 \
-    -v $(pwd)/keystore-generators/logstash.sh:/usr/share/logstash/run.sh \
-    -v $(pwd)/secrets:/usr/share/logstash/secrets \
-    docker.elastic.co/logstash/logstash:7.12.1 ./run.sh
+    -v $(pwd)/keystore-generators/logstash.sh:/opt/bitnami/logstash/bin/run.sh \
+    -v $(pwd)/secrets:/opt/bitnami/logstash/secrets \
+    docker.io/bitnami/logstash:8.3.3-debian-11-r5 ./bin/run.sh
 
 #sudo chown -R 1000:0 secrets/keystores
