f51d2f5232b989dc0c2c9d3615693b2782c76e2c
karun-singh <karun.singh@datakaveri.org>
2022-09-26 07:18:50 +0000

    ELK v7.17 bitnami charts

---
+++ K8s-deployment/Charts/elk/v7.17/keystore-generators/kibana.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+keystore="/opt/bitnami/kibana/secrets/keystores"
+passwords="/opt/bitnami/kibana/secrets/passwords"
+
+kibana-keystore create
+
+echo "kibana_system" | kibana-keystore add elasticsearch.username
+(cat "$passwords/kibana-password"; echo) | kibana-keystore add elasticsearch.password
+
+cp /opt/bitnami/kibana/config/kibana.keystore "$keystore"
