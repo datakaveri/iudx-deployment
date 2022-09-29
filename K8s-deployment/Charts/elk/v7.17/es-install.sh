f60b4cf21015a4d7bf6cb3d7ad34bc8a190a7524
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-09-29 09:35:47 +0530

    K8s: Bug fixes
    - upgrade latest auth which includes bug fix login problem
    - correct es-install
    - remove persistence in kibana
    - correct 'ClusterIP' service name in keycloak
    - Correct pghba, make pgpool service ClusterIp
    - correct redis values
    - delete extra zookeeper example resource values

--- K8s-deployment/Charts/elk/v7.17/es-install.sh
+++ K8s-deployment/Charts/elk/v7.17/es-install.sh
@@ -8,6 +8,6 @@ kubectl create secret generic elastic-credentials --from-file=elasticsearch-pass
 kubectl create secret generic elk-passwords --from-file=./secrets/passwords -n elastic
 kubectl create configmap  account-generator --from-file=./elasticsearch/account-generator.sh -n elastic
 
-helm repo add bitnami https://raw.githubusercontent.com/bitnami/charts/archive-full-index/bitnami
-helm install -f elasticsearch/es-values.yml -f elasticsearch/es-resource-values.yml -n elastic elasticsearch --version 17.9.29 $@  bitnami/elasticsearch  &&
+helm repo add bitnami https://raw.githubusercontent.com/bitnami/charts/archive-full-index/bitnami\
+helm install -f elasticsearch/es-values.yaml -f elasticsearch/es-resource-values.yaml -n elastic elasticsearch --version 17.9.29 $@  bitnami/elasticsearch
 kubectl apply -f elasticsearch/account-generator.yml -n elastic
