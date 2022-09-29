ed2fc956a29f7cc3e9aa3695d09a08ec2ab79b31
Pranav Pathak <pathakpranav90@gmail.com>
2022-09-29 10:33:25 +0530

    Updated image, update in example-secret

--- K8s-deployment/Charts/catalogue/install.sh
+++ K8s-deployment/Charts/catalogue/install.sh
@@ -3,4 +3,5 @@
 kubectl create namespace cat
 kubectl create configmap cat-env --from-env-file=./secrets/.cat.env -n cat
 kubectl create secret generic cat-config --from-file=./secrets/config.json -n cat
+kubectl create secret generic profanity-check-config --from-file=./secrets/profanity-config/config.json -n cat
 helm install catalogue-server ../catalogue -f values.yaml -f resource-values.yaml $@ -n cat
