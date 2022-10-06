a1211b171a1c4b7e0da25145d7b933ea565d6812
THARAK-RAM1 <tharakramdasari@gmail.com>
2022-10-06 12:25:47 +0530

    K8s: updated mon-stack

--- K8s-deployment/K8s-cluster/addons/mon-stack/install.sh
+++ K8s-deployment/K8s-cluster/addons/mon-stack/install.sh
@@ -14,12 +14,12 @@ helm repo add kube-state-metrics https://kubernetes.github.io/kube-state-metrics
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
-helm install --version=14.7.1 -n mon-stack prometheus -f prometheus/prometheus-values.yaml -f prometheus/resource-values.yaml prometheus-community/prometheus
+helm install --version=15.12.2 -n mon-stack prometheus -f prometheus/prometheus-values.yaml -f prometheus/resource-values.yaml prometheus-community/prometheus
 sleep 20
-helm install --version=2.6.0 -n mon-stack -f loki/loki-values.yaml -f loki/resource-values.yaml loki  grafana/loki
+helm install --version=3.0.6 -n mon-stack -f loki/loki-values.yaml -f loki/resource-values.yaml loki  grafana/loki
 sleep 20
-helm install --version=6.16.10 -f grafana/grafana-values.yaml -f grafana/resource-values.yaml  grafana -n mon-stack   grafana/grafana
+helm install --version=6.38.3 -f grafana/grafana-values.yaml -f grafana/resource-values.yaml  grafana -n mon-stack   grafana/grafana
 sleep 20
-helm install --version=3.8.1 -f promtail/promtail-values.yaml -f promtail/resource-values.yaml -n mon-stack promtail grafana/promtail
+helm install --version=6.4.0 -f promtail/promtail-values.yaml -f promtail/resource-values.yaml -n mon-stack promtail grafana/promtail
 sleep 20
 helm install --version=7.0.1 -f black-box/black-box-values.yaml -n mon-stack blackbox prometheus-community/prometheus-blackbox-exporter
