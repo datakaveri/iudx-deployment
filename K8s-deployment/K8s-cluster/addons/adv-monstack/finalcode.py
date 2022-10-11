f26b17d9c372c5aa355478822bb618cc40c4457b
THARAK-RAM1 <tharakramdasari@gmail.com>
2022-10-11 15:36:22 +0530

    K8s: updated adv-mon-stack

--- K8s-deployment/K8s-cluster/addons/adv-monstack/finalcode.py
+++ K8s-deployment/K8s-cluster/addons/adv-monstack/finalcode.py
@@ -8,15 +8,12 @@ import pytz
 
 
 def token(clientId,clientSecret,data,url):
-        url = "https://authvertx.iudx.io/auth/v1/token"
+        url = "https://authorization.iudx.org.in/auth/v1/token"
 
         headers = CaseInsensitiveDict()
         headers["clientId"] = clientId
         headers["clientSecret"] = clientSecret
         headers["Content-Type"] = "application/json"
-
-
-
         resp = requests.post(url, headers=headers, data=json.dumps(data))
         json_object = json.loads(resp.text)
         return (json_object["results"]["accessToken"])
@@ -26,10 +23,7 @@ def request(request_method,Api_Token,Api_Url,info,files,name):
         headers = CaseInsensitiveDict()
         headers["token"] = Api_Token
         headers["Conten"] = "application/json"
-        if name == "Upload external file link to a File Server" or name == "Delete external file from a File Server":
-                headers["externalStorage"] = "true"
         resp = requests.request(request_method,Api_Url, headers=headers, data=info,files=files)
-        print(name)
         return (resp)
 
 
@@ -37,40 +31,8 @@ def AMS(config,status_dict,time_dict,IST):
         archive_file_id = ""
         external_file_id = ''
         for info in config["Info"]:
-
-                if info["Test-Name"] == "Upload sample file to a File Server":
+                if info["Type"] == "private":
                         auth_token = token(config["clientID"],config["clientSecret"],info["Auth-Request-Body"],info["Auth-Server-Url"])
-                        files = [('file',('a.txt',open('/home/ubuntu/a.txt','rb'),'text/plain'))]
-                        req = request(info["Request-Method"],str(auth_token),info["Server-Url"],json.loads(info["Request-Body"]),files,info["Test-Name"])
-
-                     
-                elif info["Test-Name"] == "Upload archive file to a File Server":
-                        auth_token = token(config["clientID"],config["clientSecret"],info["Auth-Request-Body"],info["Auth-Server-Url"])
-                        files = [('file',('a.zip',open('/home/ubuntu/a.zip','rb'),'application/zip'))]
-                        req = request(info["Request-Method"],str(auth_token),info["Server-Url"],json.loads(info["Request-Body"]),files,info["Test-Name"])
-                        archive_file_id = (json.loads(req.text))["fileId"]
-                        
-                        
-        
-                elif info["Test-Name"] == "Upload external file link to a File Server":
-                        auth_token = token(config["clientID"],config["clientSecret"],info["Auth-Request-Body"],info["Auth-Server-Url"])
-                        req = request(info["Request-Method"],str(auth_token),info["Server-Url"],info["Request-Body"],"",info["Test-Name"]) 
-                        external_file_id = json.loads(req.text)["fileId"]
-
-                elif info["Test-Name"] == "Download archive file from a File Server":
-                        auth_token = token(config["clientID"],config["clientSecret"],info["Auth-Request-Body"],info["Auth-Server-Url"])
-                        req = request(info["Request-Method"],str(auth_token),info["Server-Url"]+archive_file_id,info["Request-Body"],"",info["Test-Name"])
- 
-                elif info["Test-Name"] == "Delete archive file from a File Server":
-                        auth_token = token(config["clientID"],config["clientSecret"],info["Auth-Request-Body"],info["Auth-Server-Url"])
-                        req = request(info["Request-Method"],str(auth_token),info["Server-Url"]+archive_file_id,info["Request-Body"],"",info["Test-Name"])
-                        
-                elif info["Test-Name"] == "Delete external file from a File Server":
-                        auth_token = token(config["clientID"],config["clientSecret"],info["Auth-Request-Body"],info["Auth-Server-Url"])
-                        req = request(info["Request-Method"],str(auth_token),info["Server-Url"]+external_file_id,info["Request-Body"],"",info["Test-Name"])
-
-                elif info["Type"] == "private":
-                        auth_token = token(config["clientID"],config["clientSecret"],info["Auth-Request-Body"],info["Auth-Server-Url"],)
                         req = request(info["Request-Method"],str(auth_token),info["Server-Url"],info["Request-Body"],"",info["Test-Name"])            
                 else :
                         req = request(info["Request-Method"],"",info["Server-Url"],info["Request-Body"],"",info["Test-Name"])
