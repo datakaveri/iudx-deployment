8de65ad224ae9a0e8b12d71595308ede29b32bfa
THARAK-RAM1 <tharakramdasari@gmail.com>
2022-10-12 18:56:31 +0530

    K8s: updated adv-mon-stack

--- K8s-deployment/K8s-cluster/addons/adv-monstack/finalcode.py
+++ K8s-deployment/K8s-cluster/addons/adv-monstack/finalcode.py
@@ -40,7 +40,7 @@ def AMS(config,status_dict,time_dict,IST):
                         if len(json_object["results"]) == 0:
                                 status_code_metric.labels(info["Server-Name"]+"-Array",info["Server-Url"],info["Test-Name"]).set(0)
                         else :
-                                status_code_metric.labels(info["Server-Name"]+"-Arrya",info["Server-Url"],info["Test-Name"]).set(1)
+                                status_code_metric.labels(info["Server-Name"]+"-Array",info["Server-Url"],info["Test-Name"]).set(1)
                                 
                 else :
                         req = request(info["Request-Method"],"",info["Server-Url"],info["Request-Body"],"",info["Test-Name"])
