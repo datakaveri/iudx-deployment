fd15ff6c02c5ec278dbcf2b2495e921a59347e14
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-10-13 18:02:53 +0530

    rm file server samples(a.tx,a.zip), update readme

--- K8s-deployment/K8s-cluster/addons/adv-monstack/install.sh
+++ K8s-deployment/K8s-cluster/addons/adv-monstack/install.sh
@@ -1,3 +1,3 @@
 kubectl create ns adv-monstack
-kubectl create secret generic advance-config --from-file=./a.txt --from-file=./a.zip --from-file=./secrets/adv-mon-stack-conf.json -n adv-monstack
+kubectl create secret generic advance-config  --from-file=./secrets/adv-mon-stack-conf.json -n adv-monstack
 kubectl apply -f adv-monstack.yaml
