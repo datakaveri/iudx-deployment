43489837ef7d08f8de5ea0c100f614702ada650f
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-10-13 16:09:33 +0530

    Correct Advance mon-stack
    - Rearange files
       - seperate based on swarm, docker and K8s deployment
       - rename files
    - Update Dockerfile
       - use standard workdir
       - use --no-cache in pip install -> makes images smaller
       - copy the code at end
    - Add example-secrets
    - add print statement, env variable
    - add .gitignore files

--- Docker-Swarm-deployment/single-node/adv-mon-stack/docker/adv-monstack.py
+++ Docker-Swarm-deployment/single-node/adv-mon-stack/docker/adv-monstack.py
@@ -8,7 +8,6 @@ import pytz
 
 
 def token(clientId,clientSecret,data,url):
-        url = "https://authorization.iudx.org.in/auth/v1/token"
 
         headers = CaseInsensitiveDict()
         headers["clientId"] = clientId
@@ -22,7 +21,7 @@ def request(request_method,Api_Token,Api_Url,info,files,name):
 
         headers = CaseInsensitiveDict()
         headers["token"] = Api_Token
-        headers["Conten"] = "application/json"
+        headers["Content"] = "application/json"
         resp = requests.request(request_method,Api_Url, headers=headers, data=info,files=files)
         return (resp)
 
@@ -32,25 +31,29 @@ def AMS(config,status_dict,time_dict,IST):
         external_file_id = ''
         for info in config["Info"]:
                 if info["Type"] == "private":
-                        auth_token = token(config["clientID"],config["clientSecret"],info["Auth-Request-Body"],info["Auth-Server-Url"])
+                        auth_token = token(config["clientID"],config["clientSecret"],info["Auth-Request-Body"],config["Auth-Server-Url"])
                         req = request(info["Request-Method"],str(auth_token),info["Server-Url"],info["Request-Body"],"",info["Test-Name"]) 
+                        status_code = str(req.status_code)
                 elif info["Type"] == "array_check":
                         req = request(info["Request-Method"],"",info["Server-Url"],info["Request-Body"],"",info["Test-Name"])
                         json_object = json.loads(req.text)
-                        if len(json_object["results"]) == 0:
-                                status_code_metric.labels(info["Server-Name"]+"-Array",info["Server-Url"],info["Test-Name"]).set(0)
-                        else :
-                                status_code_metric.labels(info["Server-Name"]+"-Array",info["Server-Url"],info["Test-Name"]).set(1)
+
+                         # If results array is empty, set status code to 204
+                        if req.status_code == 200 and  len(json_object["results"]) == 0:
+                                status_code = "204"
+                        else:
+                                status_code = str(req.status_code)
                                 
                 else :
                         req = request(info["Request-Method"],"",info["Server-Url"],info["Request-Body"],"",info["Test-Name"])
-                status_code = str(req.status_code)
+                        status_code = str(req.status_code)
+
                 status_code_metric.labels(info["Server-Name"],info["Server-Url"],info["Test-Name"]).set(status_code)
                 RTT = req.elapsed.total_seconds()
                 rtt_metric.labels(info["Server-Name"],info["Server-Url"],info["Test-Name"]).set(RTT)
                 curr_clock = datetime.now(IST).strftime('%H.%M')
                 api_req_time.labels(info["Server-Name"],info["Server-Url"],info["Test-Name"]).set(float(curr_clock))
-                   
+                print("Test: ", info["Test-Name"], " completed")
 
 
 
@@ -60,7 +63,7 @@ def AMS(config,status_dict,time_dict,IST):
 if __name__ == '__main__':
         prom.start_http_server(8089)
 
-with open("/home/ubuntu/adv-mon-stack-conf.json") as file:
+with open("adv-mon-stack-conf.json") as file:
     config = json.load(file)
 
 status_dict = {}
@@ -70,11 +73,11 @@ status_code_metric = prom.Gauge('http_status_code','http_status_code',["Serverna
 rtt_metric = prom.Gauge('api_rtt','api_rtt',["Servername","Url","Name"])
 api_req_time = prom.Gauge('api_req_time','api_req_time',["Servername","Url","Name"])
 IST = pytz.timezone('Asia/Kolkata')
-schedule.every(1).minutes.do(AMS,config,status_dict,time_dict,IST)
+schedule.every(config["Time"]).minutes.do(AMS,config,status_dict,time_dict,IST)
 
 
 
 while True:
-                schedule.run_pending()
-                time.sleep(1)
+        schedule.run_pending()
+        time.sleep(1)
 
