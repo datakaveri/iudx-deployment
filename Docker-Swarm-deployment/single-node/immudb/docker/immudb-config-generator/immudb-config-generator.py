366517584ba1a031d07b51abcaa05ab17fa48528
Pranav Pathak <pathakpranav90@gmail.com>
2022-11-03 17:43:34 +0530

    Update with immudb 1.4.0

--- Docker-Swarm-deployment/single-node/immudb/docker/immudb-config-generator/immudb-config-generator.py
+++ Docker-Swarm-deployment/single-node/immudb/docker/immudb-config-generator/immudb-config-generator.py
@@ -1,8 +1,21 @@
 import immudb
 import os
 from immudb import ImmudbClient
+import immudb.datatypesv2 as datatypesv2
 import urllib.request
 Host=os.environ['IMMUDB_HOST']
+
+IUDX_AUTH_FLUSH_THRESHOLD=int(os.environ['IUDX_AUTH_FLUSH_THRESHOLD'])
+IUDX_AUTH_SYNC_THRESHOLD=int(os.environ['IUDX_AUTH_SYNC_THRESHOLD'])
+IUDX_AUTH_CLEANUP_PERCENTAGE=float(os.environ['IUDX_AUTH_CLEANUP_PERCENTAGE'])
+
+IUDX_CAT_FLUSH_THRESHOLD=int(os.environ['IUDX_CAT_FLUSH_THRESHOLD'])
+IUDX_CAT_SYNC_THRESHOLD=int(os.environ['IUDX_CAT_SYNC_THRESHOLD'])
+IUDX_CAT_CLEANUP_PERCENTAGE=float(os.environ['IUDX_CAT_CLEANUP_PERCENTAGE'])
+
+IUDX_RS_FLUSH_THRESHOLD=int(os.environ['IUDX_RS_FLUSH_THRESHOLD'])
+IUDX_RS_SYNC_THRESHOLD=int(os.environ['IUDX_RS_SYNC_THRESHOLD'])
+IUDX_RS_CLEANUP_PERCENTAGE=float(os.environ['IUDX_RS_CLEANUP_PERCENTAGE'])
 
 f = open("/run/secrets/password/admin-password","r")
 ADMIN_PASSWORD = f.read()
@@ -40,17 +53,18 @@ client.createUser("iudx_rs",IUDX_RS_PASSWORD,immudb.constants.PERMISSION_RW,"iud
 client.useDatabase("iudxcat")
 client.sqlExec("CREATE TABLE auditingtable(id VARCHAR[128] NOT NULL, userRole VARCHAR[64] NOT NULL,userID VARCHAR[128] NOT NULL,iid VARCHAR[250] NOT NULL,api VARCHAR[128] NOT NULL,method VARCHAR[32] NOT NULL,time INTEGER NOT NULL,iudxID VARCHAR[256] NOT NULL,PRIMARY KEY id);")
 client.sqlExec("CREATE INDEX ON auditingtable(userID, iudxID, time, api);")
+client.updateDatabaseV2(b"iudxcat", datatypesv2.DatabaseSettingsV2(indexSettings=datatypesv2.IndexSettings( flushThreshold=IUDX_CAT_FLUSH_THRESHOLD, syncThreshold=IUDX_CAT_SYNC_THRESHOLD, cleanupPercentage=IUDX_CAT_CLEANUP_PERCENTAGE),))
+
 client.useDatabase("iudxauth")
 client.sqlExec("CREATE TABLE table_auditing(id VARCHAR[128] NOT NULL, body VARCHAR NOT NULL,userid VARCHAR[128] NOT NULL,endpoint VARCHAR[128] NOT NULL,method VARCHAR[32] NOT NULL,time INTEGER NOT NULL,PRIMARY KEY id);")
-
 client.sqlExec("CREATE INDEX ON table_auditing(userid, endpoint, time);")
+client.updateDatabaseV2(b"iudxauth", datatypesv2.DatabaseSettingsV2(indexSettings=datatypesv2.IndexSettings( flushThreshold=IUDX_AUTH_FLUSH_THRESHOLD, syncThreshold=IUDX_AUTH_SYNC_THRESHOLD,cleanupPercentage=IUDX_AUTH_CLEANUP_PERCENTAGE),))
+
+
 client.useDatabase("iudxrsorg")
-
-
 client.sqlExec("CREATE TABLE rsaudit (id VARCHAR[128] NOT NULL,api VARCHAR[128] NOT NULL,userid VARCHAR[128] NOT NULL,epochtime INTEGER NOT NULL,resourceid VARCHAR[256] NOT NULL,isotime VARCHAR[64] NOT NULL,providerid VARCHAR[128] NOT NULL,size INTEGER, PRIMARY KEY id);")
 client.sqlExec("CREATE INDEX ON rsaudit(userid, epochtime, providerid, resourceid);")
-
-
+client.updateDatabaseV2(b"iudxrsorg", datatypesv2.DatabaseSettingsV2(indexSettings=datatypesv2.IndexSettings(flushThreshold=IUDX_RS_FLUSH_THRESHOLD, syncThreshold=IUDX_RS_SYNC_THRESHOLD, cleanupPercentage=IUDX_RS_CLEANUP_PERCENTAGE),))
 
 print(client.listUsers())
 print(client.databaseList())
