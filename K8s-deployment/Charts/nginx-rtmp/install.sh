dfa31c13b8cc412b200c85be33f5330415d7d51f
tharak.1242 <tharak.ram@datakaveri.org>
2022-12-13 14:59:47 +0530

    K8s: modified nginx-rtmp and videoserver bash files

--- K8s-deployment/Charts/nginx-rtmp/install.sh
+++ K8s-deployment/Charts/nginx-rtmp/install.sh
@@ -4,4 +4,4 @@ docker build -t ghcr.io/datakaveri/nginx-rtmp:v1 .
 docker push ghcr.io/datakaveri/nginx-rtmp:v1
 kubectl create ns nginx-rtmp
 kubectl create secret generic certificates  --from-file=./privateECDSASHA256.pem  --from-file=./pubECDSASHA256.pem -n nginx-rtmp
-kubectl apply -f deployment.yaml
+kubectl apply -f deployment.yaml -n nginx-rtmp
--- K8s-deployment/Charts/videoserver-apps/install.sh
+++ K8s-deployment/Charts/videoserver-apps/install.sh
@@ -1,4 +1,4 @@
 docker build -t ghcr.io/datakaveri/video-server-apps:v1 .
 docker push ghcr.io/datakaveri/video-server-apps:v1
-kubectl create secret topics --from-file=./topics.json
+kubectl create secret topics --from-file=./topics.json -n kafka
 kubectl apply -f jobs.yaml -n kafka
