7e675d320da184fbef5bb3a3932467f6421a4e20
tharak.1242 <tharak.ram@datakaveri.org>
2022-12-13 14:48:58 +0530

    K8s: Added nginx-rtmp module

---
+++ K8s-deployment/Charts/nginx-rtmp/install.sh
@@ -0,0 +1,7 @@
+openssl ecparam -name secp256k1 -genkey -noout -out privateECDSASHA256.pem
+openssl ec -in privateECDSASHA256.pem -pubout > pubECDSASHA256.pem
+docker build -t ghcr.io/datakaveri/nginx-rtmp:v1 .
+docker push ghcr.io/datakaveri/nginx-rtmp:v1
+kubectl create ns nginx-rtmp
+kubectl create secret generic certificates  --from-file=./privateECDSASHA256.pem  --from-file=./pubECDSASHA256.pem -n nginx-rtmp
+kubectl apply -f deployment.yaml
