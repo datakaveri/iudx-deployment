55c620e365809f883ffcc3b4fb3c237288812d8e
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2022-12-14 22:48:17 +0530

    generate erlang cookie through script

--- K8s-deployment/Charts/databroker/create-secrets.sh
+++ K8s-deployment/Charts/databroker/create-secrets.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 50) > secrets/credentials/rabbitmq-erlang-cookie
 echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 30) > secrets/credentials/admin-password
 echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 20) > secrets/credentials/rs-password
 echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 20) > secrets/credentials/fs-password 
