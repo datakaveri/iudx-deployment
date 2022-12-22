543586a18c76497a2f48a566be5a0813c38ad1db
karun-singh <karun.singh@datakaveri.org>
2022-12-23 01:41:33 +0530

    updates and fixes

--- K8s-deployment/Charts/elk/elasticsearch/generate-certs.sh
+++ K8s-deployment/Charts/elk/elasticsearch/generate-certs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 docker run  --rm  -it  -v $(pwd)/secrets:/tmp/secrets \
-  -u 1000  docker.elastic.co/elasticsearch/elasticsearch:8.3.3\
+  -u 0  docker.elastic.co/elasticsearch/elasticsearch:8.3.3\
                 /bin/sh -c " \
                         elasticsearch-certutil ca --out /tmp/secrets/pki/elastic-stack-ca.p12 --pass '' && \
                         elasticsearch-certutil cert --name security-master  --ca /tmp/secrets/pki/elastic-stack-ca.p12 --pass '' --ca-pass '' --out /tmp/secrets/pki/elastic-certificates.p12"
