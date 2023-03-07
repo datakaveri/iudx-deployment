929563461e88b283e522d6b6f68cd3c9bf7fda3a
karun-singh <karun.singh@datakaveri.org>
2023-03-07 13:51:42 +0530

    update script comment

--- K8s-deployment/K8s-cluster/addons/velero/aws/velero-change-aws-az.sh
+++ K8s-deployment/K8s-cluster/addons/velero/aws/velero-change-aws-az.sh
@@ -15,7 +15,7 @@ sed -i "s/$OLD_ZONE/$NEW_ZONE/g" $BACKUP_NAME-volumesnapshots.json
 gzip $BACKUP_NAME-volumesnapshots.json
 aws s3 cp $BACKUP_NAME-volumesnapshots.json.gz s3://$BUCKET_NAME/backups/$BACKUP_NAME/$BACKUP_NAME-volumesnapshots.json.gz
 
-# Bigger file
+# PV manifests
 aws s3 cp s3://$BUCKET_NAME/backups/$BACKUP_NAME/$BACKUP_NAME.tar.gz .
 mkdir $BACKUP_NAME-temp
 tar -xvf $BACKUP_NAME.tar.gz -C $BACKUP_NAME-temp
