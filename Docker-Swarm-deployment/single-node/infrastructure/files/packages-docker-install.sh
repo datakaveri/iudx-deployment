a6141ca09d3399d49db4f11cd737c7e9283de161
abhi4578 <29863835+abhi4578@users.noreply.github.com>
2023-04-18 09:49:06 +0530

    minor fix in docker install

--- Docker-Swarm-deployment/single-node/infrastructure/files/packages-docker-install.sh
+++ Docker-Swarm-deployment/single-node/infrastructure/files/packages-docker-install.sh
@@ -9,8 +9,8 @@ echo "installed 'net-tools iputils-ping dnsutils htop vim' packages at host: `ho
 apt-get install -y --no-install-recommends \
 ca-certificates curl gnupg  &> /dev/null 
 install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
