afafa7b8435260d39741e687c3096769ca5a3bbb
Sridhar Rao Kukkala <sridhar.rao@datakaveri.org>
2024-03-21 19:54:33 +0530

    changes added

--- K8s-deployment/Charts/postgresql/create-secrets.sh
+++ K8s-deployment/Charts/postgresql/create-secrets.sh
@@ -8,7 +8,7 @@ echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 30) > secrets/passwor
 echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 30) > secrets/passwords/postgres-auth-password
 echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 30) > secrets/passwords/postgres-rs-password
 echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 30) > secrets/passwords/postgres-keycloak-password
-echo -n "postgre;postgres;iudx_rs_user;iudx_auth_user;iudx_keycloak_user" > secrets/passwords/usernames
-cp secrets/passwords/postgresql-password secrets/passwords/password
-echo -n "$(cat secrets/passwords/password);$(cat secrets/passwords/postgresql-password);$(cat secrets/passwords/postgres-rs-password);$(cat secrets/passwords/postgres-auth-password);$(cat secrets/passwords/postgres-keycloak-password)" > secrets/passwords/passwords
+echo -n "postgres;iudx_rs_user;iudx_auth_user;iudx_keycloak_user" > secrets/passwords/usernames
+
+echo -n "$(cat secrets/passwords/postgresql-password);$(cat secrets/passwords/postgres-rs-password);$(cat secrets/passwords/postgres-auth-password);$(cat secrets/passwords/postgres-keycloak-password)" > secrets/passwords/passwords
 
