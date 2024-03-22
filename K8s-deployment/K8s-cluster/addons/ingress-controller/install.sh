f36912f5f0ce4fe50843ec0a407251ead290c570
karun-singh <karun.singh@datakaveri.org>
2024-03-22 12:39:48 +0530

    added cos-root-files for nginx

--- K8s-deployment/K8s-cluster/addons/ingress-controller/install.sh
+++ K8s-deployment/K8s-cluster/addons/ingress-controller/install.sh
@@ -2,6 +2,7 @@
 
 kubectl create namespace ingress-nginx && kubectl create namespace memcached
 kubectl create configmap  error-conf --from-file=./ingress-nginx/conf/error.conf -n ingress-nginx 
+kubectl create configmap  cos-root  --from-file=ingress-nginx/html/ -n ingress-nginx
 helm repo update ingress-nginx bitnami
 helm install --version=4.2.5 nginx ingress-nginx/ingress-nginx  -f ingress-nginx/nginx-values.yaml -f ingress-nginx/resource-values.yaml -n ingress-nginx && sleep 10 
 helm install memcached bitnami/memcached --version=6.2.4 -f memcached/memcached-values.yaml -f memcached/resource-values.yaml -n memcached
