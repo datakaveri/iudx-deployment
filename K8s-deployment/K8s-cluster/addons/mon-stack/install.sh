ba2422806306853a5ae326f0b3a9a708686c4184
karun-singh <karun.singh@datakaveri.org>
2024-03-25 19:03:34 +0530

    updated grafana chart values

--- K8s-deployment/K8s-cluster/addons/mon-stack/install.sh
+++ K8s-deployment/K8s-cluster/addons/mon-stack/install.sh
@@ -15,7 +15,7 @@ helm install --version=25.8.2 -n mon-stack prometheus -f prometheus/prometheus-v
 sleep 20
 helm install --version=5.42.0 -n mon-stack -f loki/loki-values.yaml -f loki/resource-values.yaml loki  grafana/loki
 sleep 20
-helm install --version=6.38.3 -f grafana/grafana-values.yaml -f grafana/resource-values.yaml  grafana -n mon-stack   grafana/grafana
+helm install --version=7.2.5 -f grafana/grafana-values.yaml -f grafana/resource-values.yaml  grafana -n mon-stack   grafana/grafana
 sleep 20
 helm install --version=6.4.0 -f promtail/promtail-values.yaml -f promtail/resource-values.yaml -n mon-stack promtail grafana/promtail
 sleep 20
