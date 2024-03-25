5e77ae973dab2c32787b761551d58fa4af6a2569
SRINI2410 <sreesri0605@gmail.com>
2024-03-25 10:55:25 +0530

    bugs fix

--- K8s-deployment/Charts/keycloak/create-secrets.sh
+++ K8s-deployment/Charts/keycloak/create-secrets.sh
@@ -2,4 +2,3 @@
 
 echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/admin-password
 cp ../postgresql/secrets/passwords/postgres-keycloak-password   secrets/db-password
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 16) > secrets/admin-username
