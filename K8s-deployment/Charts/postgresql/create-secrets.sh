a3598d650da1e728bd0e8c8e94d6c07dfa2180c6
Pranav <Pranav Pathak>
2024-04-23 15:35:04 +0530

    postgres changes to 5.5.0

--- K8s-deployment/Charts/postgresql/create-secrets.sh
+++ K8s-deployment/Charts/postgresql/create-secrets.sh
@@ -8,6 +8,8 @@ echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 30) > secrets/passwor
 echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 30) > secrets/passwords/postgres-auth-password
 echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 30) > secrets/passwords/postgres-rs-password
 echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 30) > secrets/passwords/postgres-keycloak-password
-echo -n "postgres;iudx_rs_user;iudx_auth_user;iudx_keycloak_user" > secrets/passwords/usernames
-echo -n "$(cat secrets/passwords/postgresql-password);$(cat secrets/passwords/postgres-rs-password);$(cat secrets/passwords/postgres-auth-password);$(cat secrets/passwords/postgres-keycloak-password)" > secrets/passwords/passwords
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 30) > secrets/passwords/postgres-acl-apd-password
+echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 30) > secrets/passwords/postgres-dmp-apd-password
+echo -n "postgres;iudx_rs_user;iudx_auth_user;iudx_keycloak_user;iudx_acl_apd_user;iudx_dmp_apd_user" > secrets/passwords/usernames
+echo -n "$(cat secrets/passwords/postgresql-password);$(cat secrets/passwords/postgres-rs-password);$(cat secrets/passwords/postgres-auth-password);$(cat secrets/passwords/postgres-keycloak-password);$(cat secrets/passwords/postgres-dmp-apd-password);$(cat secrets/passwords/postgres-acl-apd-password)" > secrets/passwords/passwords
 
