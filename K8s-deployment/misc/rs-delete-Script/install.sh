7567b8267127278cedc31ad803b867477d713294
Pranav <Pranav Pathak>
2024-06-03 16:27:17 +0530

    removed old rs-delete-script

--- K8s-deployment/misc/rs-delete-Script/install.sh
+++
@@ -1,3 +0,0 @@
-kubectl create ns delete-script
-kubectl create secret generic delete-script-config --from-file=./secrets/script-config.json -n delete-script
-kubectl apply -f Deployment.yaml -n delete-script
