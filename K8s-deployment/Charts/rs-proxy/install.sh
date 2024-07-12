ad3ec7e4f2f434f2f0046e9174229b7df43e61d7
Srini <sreesri0605@gmail.com>
2024-07-12 17:10:12 +0530

    Update install.sh script

--- K8s-deployment/Charts/rs-proxy/install.sh
+++ K8s-deployment/Charts/rs-proxy/install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-kubectl create namespace adex-rs-proxy
-kubectl create configmap rs-proxy-env --from-env-file=./secrets/.rs-proxy.env -n adex-rs-proxy
-kubectl create secret generic rs-proxy-cert --from-file=./secrets/Test-Class2DocumentSigner2014.pfx -n adex-rs-proxy
-kubectl create secret generic rs-proxy-config --from-file=./secrets/config.json -n adex-rs-proxy
-helm install rs-proxy ../rs-proxy -f values.yaml -f resource-values.yaml $@ -n adex-rs-proxy
+kubectl create namespace rs-proxy
+kubectl create configmap rs-proxy-env --from-env-file=./secrets/.rs-proxy.env -n rs-proxy
+kubectl create secret generic rs-proxy-cert --from-file=./secrets/Test-Class2DocumentSigner2014.pfx -n rs-proxy
+kubectl create secret generic rs-proxy-config --from-file=./secrets/config.json -n rs-proxy
+helm install rs-proxy ../rs-proxy -f values.yaml -f resource-values.yaml $@ -n rs-proxy
