b48cc5fb33649ea8de6c2a22f90746d42e45dd6f
Siva Naik <siva.naik@datakaveri.org>
2024-09-12 12:41:11 +0530

    changed the mc command to set bucket public and password to alphanumeric

--- Docker-Swarm-deployment/single-node/minio/create-secrets.sh
+++ Docker-Swarm-deployment/single-node/minio/create-secrets.sh
@@ -2,5 +2,5 @@
 set -e 
 
 mkdir -p secrets/passwords
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/passwords/minio-username
-echo -n  $(cat /dev/urandom | tr -dc 'a-zA-Z0-9-!@#$%^&*+|:<>?' | head -c 16) > secrets/passwords/minio-password
+echo -n $(cat /dev/urandom | tr -dc 'a-zA-Z' | head -c 16) > secrets/passwords/minio-username
+echo -n $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 16) > secrets/passwords/minio-password
