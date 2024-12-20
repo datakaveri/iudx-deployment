6deba0c8e3ef5518c55a975567a6776a04df72cf
karun-singh <karun.singh@datakaveri.org>
2024-12-20 14:10:35 +0530

    fhir rs updates and fixes

--- K8s-deployment/Charts/fhir-resource-server/install.sh
+++ K8s-deployment/Charts/fhir-resource-server/install.sh
@@ -2,4 +2,4 @@ kubectl create namespace icmr-fhir-rs
 kubectl create configmap fhir-rs-env --from-env-file=secrets/.fhir-rs.env -n icmr-fhir-rs
 kubectl create secret generic fhir-rs-config --from-file=secrets/config.json -n icmr-fhir-rs
 kubectl create secret generic application-yaml --from-file=secrets/application.yaml -n icmr-fhir-rs
-helm install fhir-rs -f values.yaml -f rs-values.yaml ../fhir-resource-server/ $@ -n icmr-fhir-rs
+helm install fhir-rs -f values.yaml -f resource-values.yaml ../fhir-resource-server/ $@ -n icmr-fhir-rs
