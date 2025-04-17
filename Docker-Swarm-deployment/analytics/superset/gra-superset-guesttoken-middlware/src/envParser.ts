ca1c421869a75bcafbc6b98fe2ac20da0d1d76fe
Srini <you@example.com>
2025-04-17 13:28:58 +0100

    move dockerfile(s) to root docker dir

--- Docker-Swarm-deployment/analytics/superset/gra-superset-guesttoken-middlware/src/envParser.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-
-export const envSchema = z.object({
-	SUPERSET_URL: z.string(),
-	SUPERSET_ADMIN: z.string(),
-	SUPERSET_ADMIN_PASSWORD: z.string(),
-	APP_PORT: z.string().transform((val) => parseInt(val, 10)),
-});
